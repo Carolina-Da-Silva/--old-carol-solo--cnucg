863ea0fd3de450b1322813523eb17cc56e6353a8
Carol <143283766+Carolina-Da-Silva@users.noreply.github.com>
2024-04-25 12:08:55 +0000

    apr 25

--- cnucg-ppo/src/App.tsx
+++ cnucg-ppo/src/App.tsx
@@ -2,12 +2,21 @@ import "./main.css"
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App() {
 
   return <>
     <Header />
-    <Home />
+    <main>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<>A Carol é ótima excelente!</>} />
+        </Routes>
+      </BrowserRouter>
+    </main>
     <Footer />
   </>
 }
--- cnucg-ppo/src/components/Footer.tsx
+++ cnucg-ppo/src/components/Footer.tsx
@@ -15,13 +15,12 @@ export default function () {
                 <img src={fb} alt="" /></a>
             </div>
             <div className="outros">
-                <div>Termos de uso</div>
-                <div>Política de privacidade</div>
+                <div className="termos"><a href="#">Termos de uso</a></div>  
+                <div className="privacidade"><a href="#">Política de privacidade</a></div>
             </div>
             <div className="informacoes">
                 <a href="https://www.camboriu.ifc.edu.br/">Instituto Federal de Educação, Ciência e Tecnologia Catarinense - Campus Camboriú</a>
-                <div className="endereço">Rua: Joaquim Garcia, s/c - CEP 88340-055 - Camboriú - SC </div>
-                <div className="contato">Fone: (47) 2104-0800 </div>
+                <div className="endereço">Rua: Joaquim Garcia, s/c - CEP 88340-055 - Camboriú - SC / Fone: (47) 2104-0800</div>
             </div>
         </footer>
     </>
