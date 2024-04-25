08728c33d22c68d889370d90e94eea0cd7a865da
Carol <143283766+Carolina-Da-Silva@users.noreply.github.com>
2024-04-25 02:29:25 +0000

    24/abr

--- cnucg-ppo/src/components/Footer.tsx
+++ cnucg-ppo/src/components/Footer.tsx
@@ -14,12 +14,15 @@ export default function () {
                 <a href="https://www.facebook.com/ifccamboriu.oficial/?locale=lv_LV&_rdr">
                 <img src={fb} alt="" /></a>
             </div>
-            <div>
+            <div className="outros">
+                <div>Termos de uso</div>
+                <div>Política de privacidade</div>
+            </div>
+            <div className="informacoes">
                 <a href="https://www.camboriu.ifc.edu.br/">Instituto Federal de Educação, Ciência e Tecnologia Catarinense - Campus Camboriú</a>
                 <div className="endereço">Rua: Joaquim Garcia, s/c - CEP 88340-055 - Camboriú - SC </div>
                 <div className="contato">Fone: (47) 2104-0800 </div>
             </div>
-            
         </footer>
     </>
 }
