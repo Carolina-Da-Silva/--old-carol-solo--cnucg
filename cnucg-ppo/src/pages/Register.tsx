8c10b8ad0cdbfd77418f207e8a0638b5c5d4960d
Carol <143283766+Carolina-Da-Silva@users.noreply.github.com>
2024-04-26 02:52:39 +0000

    25 abr

--- cnucg-ppo/src/pages/Register.tsx
+++ cnucg-ppo/src/pages/Register.tsx
@@ -2,6 +2,25 @@ import "./Register.css"
 
 export default function () {
     return <>
-    <div>texto</div>
+        <div className="cadastro">
+            <div className="titulo_cad">
+                <div>Cadastro de usuário do cão guia</div>
+            </div>
+            <div className="sub_titulo">
+                <div>O projeto de cão-guia oferece um treinamento especializado para preparar cães que irão auxiliar pessoas com deficiência visual, garantindo-lhes independência e mobilidade na sua rotina.</div>
+            </div>
+            <div className="pessoa">
+                <div className="pf">
+                    <div>Pessoa Física</div>
+                </div>
+                <div className="pj">
+                    <div>Pessoa Jurídica</div>
+                </div>
+            </div>
+            <div className="bt">
+                <button className="bt_pf"></button>
+                <button className="bt_pj"></button>
+            </div>
+        </div>
     </>
 }
