a3040705f873452a68eafd3aeb27edf1b6e7cf80
Carol <143283766+Carolina-Da-Silva@users.noreply.github.com>
2024-05-02 13:02:02 +0000

    asdasd

--- cnucg-ppo/src/pages/Home.tsx
+++ cnucg-ppo/src/pages/Home.tsx
@@ -1,12 +1,21 @@
-import "./Home.css"
 import Tile from "../components/Tile"
+import styled from "styled-components"
+
+const HomeContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: 20px;
+    height: 100%;
+`
 
 export default function () {
     return <>
-        <div className="home-container">
+        <HomeContainer>
             <Tile icon="qr_code_2" title="Validar Carteira Nacional" button="Quero verificar a autenticidade de uma carteina nacional de usuário de cão guia" />
             <Tile icon="sound_detection_dog_barking" title="Novo cadastro" button="Sou usuário de cão guia e quero me cadastrar para obter a carteina nacional de usuário de cão guia" />
             <Tile icon="assignment_ind" title="Acesso" button="Acesse o sistema para as demais funcionalidades!" />
-        </div>
+        </HomeContainer>
     </>
 }
